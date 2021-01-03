abfdbdff6d997185736c0809dfd116c835eadbb3
rishya-prabakaran <60148918+rishya-prabakaran@users.noreply.github.com>
2021-01-03 22:56:47 +0530

    Add files via upload

---
+++ admin/includes/side_wrapper.php
@@ -0,0 +1,43 @@
+<div class="sidebar-wrapper" style="background: #D77085">
+            <div class="logo">
+                <a href="index.php" class="simple-text" style="font-family:Verdana; color:Black;">
+                   <b> FooDelicious</b>
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="food_list.php">
+                        <i class="pe-7s-graph"></i>
+                        <p>Menu</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="food_add.php">
+                        <i class="pe-7s-note"></i>
+                        <p>Add dish</p>
+                    </a>
+                </li>
+				
+                <li>
+                    <a href="orders.php">
+                        <i class="pe-7s-note2"></i>
+                        <p>Orders</p>
+                    </a>
+                </li>
+				<li>
+                    <a href="reservations.php">
+                        <i class="pe-7s-note2"></i>
+                        <p>Table reservations</p>
+                    </a>
+                </li>
+				<!--<li>
+                    <a href="#">
+                        <i class="pe-7s-news-paper"></i>
+                        <p>Gallery</p>
+                    </a>
+                </li>-->
+                
+            </ul>
+    	</div>
+    </div>
