b9a2852da9b2b73d82073f4666eed8e6e965a48d
rishya-prabakaran <60148918+rishya-prabakaran@users.noreply.github.com>
2021-04-21 16:21:32 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -213,6 +213,19 @@
 	</div>
 	
 </div>
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/607fc17c62662a09efc09257/1f3pgnlb6';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 	
 </body>
 
